ad09c59a565e6532f43410edf0b6172583bc967b
mwacharo <mwacharommwanyolo@gmail.com>
2025-08-13 17:36:38 +0300

    commit

--- routes/console.php
+++ routes/console.php
@@ -14,7 +14,7 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 
-Schedule::command('agents:mark-offline')->everyMinute();
+Schedule::command('app:mark-agents-offline')->everyMinute();
 // Artisan::command('app:mark-agents-offline', function () {
 //     $this->call('app:mark-agents-offline');
 // })->describe('Mark agents as offline if they have been inactive for more than 1 minute');
