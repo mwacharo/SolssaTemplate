1d80b814c60d456ecfb860dc6df709923d14283b
mwacharo <mwacharommwanyolo@gmail.com>
2025-10-26 10:55:03 +0300

    commit

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,9 @@ return [
     // 'allowed_origins' => ['*'],
 
        'allowed_origins' => [
+        'https://mwamkoherbs.com',
         'https://mwamkoherbs.vercel.app',
+
         // 'http://localhost:5174',
         // 'http://127.0.0.1:5174',
         // 'http://localhost:5173',
