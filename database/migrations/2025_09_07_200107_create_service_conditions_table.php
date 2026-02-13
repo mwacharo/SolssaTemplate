bef7ac7178deea75cb9bfe5c97a90a37f0dfbcce
YOUR NAME <YOUR EMAIL>
2026-02-13 19:02:17 +0300

    commit

--- database/migrations/2025_09_07_200107_create_service_conditions_table.php
+++ database/migrations/2025_09_07_200107_create_service_conditions_table.php
@@ -6,29 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('php artisan make:migration add_operator_rate_type_value_priority_to_service_conditions_table --table=service_conditions
-', function (Blueprint $table) {
+        Schema::create('service_conditions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('service_id')->constrained()->cascadeOnDelete();
-            // $table->string('condition_type'); // e.g., weight, distance, region, flat_rate
             $table->foreignId('condition_type_id')->constrained()->cascadeOnDelete();
             $table->decimal('min_value', 12, 2)->nullable();
             $table->decimal('max_value', 12, 2)->nullable();
             $table->decimal('rate', 12, 2)->default(0);
-            $table->string('unit')->nullable(); // kg, km, %, flat
+            $table->string('unit')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('service_conditions');
