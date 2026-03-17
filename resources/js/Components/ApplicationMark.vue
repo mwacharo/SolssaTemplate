9be5323584ae5ee27141a62c79a5a305a1eb602a
YOUR NAME <YOUR EMAIL>
2026-03-18 00:42:51 +0300

    commit

--- resources/js/Components/ApplicationMark.vue
+++ resources/js/Components/ApplicationMark.vue
@@ -34,6 +34,7 @@ Status at Jan 21, 2026, 11:14 AM
  -->
 
     <!-- <img src="/images/rushbin-logo.png" alt="Company Logo" class="logo-img" /> -->
+    <img src="/images/rushbin.jpeg" alt="Company Logo" class="logo-img" />
 </template>
 
 <style>
